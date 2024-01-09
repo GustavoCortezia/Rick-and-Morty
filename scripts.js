2b3388c75379dc36604fb8146ff81f95263a1ed5
Gustavo Cortezia <gus.cortezia@gmail.com>
2024-01-09 11:54:14 -0300

    script funcionando

--- scripts.js
+++ scripts.js
@@ -1,64 +1,149 @@
-lista = document.getElementById("lista")
+let lista = document.getElementById("lista")
+let paginacao = document.getElementById("paginacao")
+
 let personagens = [];
 
-async function getPersonagens(){
-    axios.get("https://rickandmortyapi.com/api/character").then ((result) => {
-       personagens.push(result.data.results);
-
-       criaCard()
-
-        console.log(personagens.length);
-
-       console.log(personagens);
-    }).catch((err) => {
-        console.log(err);
-    })
-}
-function criaCard() {
-    // console.log(personagens[1].name);
-    console.log(personagens[0].length);
-    // console.log(personagens.info.count);
-
-    personagens.forEach(personagem => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-
-        card.innerHTML = `
-        <img src="${personagem.image}" alt="" srcset="">
-
-        <h4 class="teste">${personagem.name}</h4>
-
-        <h4>Status: ${personagem.status}</h4>
-
-        <h4>Espécie: ${personagem.species}</h4>
-        </div>
-        `
-    });
-}
+let currentPage = 1;
+let totalPages = 0;
 
 
-async function footerInfo() {
+//CONTEUDO
+
+async function getPersonagens(page) {
     try {
-      const responseChar = (await api.get('character')).data
-      const responseLoc = (await api.get('character')).data
-      const responseEp = (await api.get('character')).data
+        const response = await api.get(`/character?page=${page}`);
+        const characters = response.data.results;
+        criaCard(characters);
+    } catch (err) {
+        console.error('ERRO AO ENCONTRAR OS PERSONAGENS: ', err);
+    }
+}
 
-      const characters = responseChar.info.count
-      const locations = responseLoc.info.count
+function criaCard(characters) {
+    lista.innerHTML = '';
+    
+    characters.forEach(async character => {
+        const card = document.createElement('div');
+        const infos = document.createElement('div');
+        card.classList.add('card')
+        infos.classList.add('infos')
 
-    const foooterPersonagens = document.getElementById("footerPerson");
+        const response = await api.get(character.episode[character.episode.length - 1])
+        const episodeLink = response.data.name
 
-    foooterPersonagens.innerHTML = `${characters}`
+            card.innerHTML = `
+            <img class="imagemCard" src="${character.image}">`
+            infos.innerHTML = `
+                <h2 class="nome" > ${character.name}</h2>
+                <h3 class="status" >${character.status} - ${character.species}</h3>
+                <p class="ultimaLocal">
+                    Última localização conhecida <br>
+                    <b> ${character.location.name} </b> </p>
+                <p class="ultimoEP">
+                    Visto a última vez em: <b> ${episodeLink} </b> </p>
+                `
 
-    } catch(err) {
-        console.log(err);
+            card.append(infos);
+            lista.append(card); 
+        });
+
+}
+
+function renderizarConteudo(data) {
+    lista.innerHTML = '';
+    if (buscando === true) {
+        criaCard(data);
+    } 
+    else {
+        criaCard(data);
+        botaoPaginacao();
+    }
+}
+
+// PAGINAÇÃO
+
+function botaoPaginacao() {
+    paginacao.innerHTML = '';
+
+    const botaoAnterior = document.createElement('button');
+    botaoAnterior.textContent = 'Anterior';
+
+    const botaoProximo = document.createElement('button');
+    botaoProximo.textContent = 'Proximo';
+
+
+    botaoAnterior.addEventListener('click', () => anterior());
+    paginacao.appendChild(botaoAnterior);
+ 
+    botaoProximo.addEventListener('click', () => proximo());
+    paginacao.appendChild(botaoProximo);
+}
+
+async function proximo() {
+    const totalPages = 42;
+    if (currentPage < totalPages) {
+        currentPage++;
+        await getPersonagens(currentPage);
     }
 }
 
 
+async function anterior() {
+    if (currentPage >= 2) {
+        currentPage--;
+        await getPersonagens(currentPage);
+    }
+}
 
 
+let buscando = false;
+async function procuraPersonagem() {
+    const busca = document.getElementById('busca').value;
+    if (busca !== '') {
+        buscando = true;
+
+        try {
+            const response = await api.get(`/character?name=${busca}`);
+            renderizarConteudo(response.data.results);
+        } catch (err) {
+            console.error('Erro ao pesquisar personagens', err);
+        }
+    } 
+    else {
+        buscando = false;
+        await getPersonagens(currentPage);
+    }
+}
+
+//FOOTER
+
+async function footerInfo() {
+    try { 
+        const characters = (await api.get('character')).data.info.count
+        const locations = (await api.get('location')).data.info.count
+        const episodes = (await api.get('episode')).data.info.count
+        
+        const foooterPersonagens = document.getElementById("footerPerson");
+        const footerLocations = document.getElementById("footerLocal");
+        const foooterEpisodes = document.getElementById("footerEp");
+
+        foooterPersonagens.innerText = `${characters}`
+        footerLocations.innerText = `${locations}`
+        foooterEpisodes.innerText = `${episodes}`
+
+    } catch(err) {
+        console.log('Erro ao coletar as informações', err);
+    }
+}
 
 
+// INICIAR PROGRAMA
 
-getPersonagens()
+async function start() {
+    await getPersonagens(currentPage);
+    botaoPaginacao();
+    footerInfo();
+}
+
+
+start();
